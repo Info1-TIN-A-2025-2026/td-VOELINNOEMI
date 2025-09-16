6167c48a96339673254d3d42aaf10fb60b330bce
VOELINNOEMI <noemi.voelin@heig-vd.ch>
2025-09-16 12:32:32 +0000

    added T20250916

---
+++ TD20250916/td.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_students = 23; //entier non signé
+	// signed int m = -42; //signed optionnel
+	int account_value = -42; //faire bien attention au nom des variables
+	float temperature = 18.1;  // nbr réels / 32 bits
+	double pi = 3.1415;	 // plus précis sur 64 bits
+}
